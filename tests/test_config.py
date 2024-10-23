037fcea5d5193c0761596b57778763bbfe0988f1
Jack Butler <jackbtlr@amazon.com>
2024-10-23 13:48:54 +0100

    add validation and test to ensure diverse models (currently only supporting multiple different models)

--- tests/test_config.py
+++ tests/test_config.py
@@ -14,3 +14,22 @@ def should_allow_arbitrary_modelids(model_ids: list[str]):
     )
     assert hive_config.bedrock_model_ids == model_ids
     assert hive_config.aggregator_model_id == model_ids[0]
+
+
+@pytest.mark.parametrize("num_reflections", [(-5), (-1)])
+def should_raise_error_for_invalid_num_reflections(num_reflections: int):
+    with pytest.raises(ValueError):
+        HiveConfig(
+            bedrock_model_ids=["FAKE_MODEL"],
+            num_reflections=num_reflections,
+            aggregator_model_id="FAKE_MODEL",
+        )
+
+
+def should_raise_error_for_duplicate_model_ids():
+    with pytest.raises(ValueError):
+        HiveConfig(
+            bedrock_model_ids=["FAKE_MODEL", "FAKE_MODEL"],
+            num_reflections=10,
+            aggregator_model_id="FAKE_MODEL",
+        )
