1047ddf6c9e6bda4f3c5fb3ad939cb3687661d28
Jack Butler <jackbtlr@amazon.com>
2024-10-28 15:01:35 +0000

    add centralised control of logger level

--- src/bhive/__init__.py
+++ src/bhive/__init__.py
@@ -3,5 +3,15 @@ from loguru import logger
 from bhive.client import Hive as Hive
 from bhive.config import HiveConfig as HiveConfig
 
-logger.remove()
-logger.add(sys.stderr, level="INFO")  # users can change this to DEBUG
+LOGGER_LEVELS = ["TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"]
+
+
+def set_logger_level(level: str) -> None:
+    # users can change this to DEBUG
+    if level not in LOGGER_LEVELS:
+        raise ValueError(f"Invalid logger level: {level}, must be one of {LOGGER_LEVELS}")
+    logger.remove()
+    logger.add(sys.stderr, level=level)
+
+
+set_logger_level("INFO")
