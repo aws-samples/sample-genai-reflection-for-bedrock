4906359473e9c421fc9a6add4da0706f4c8c2908
Nikita Kozodoi <kozodoi@amazon.de>
2024-12-06 15:59:32 +0100

    📊 Update token prices

--- src/bhive/cost.py
+++ src/bhive/cost.py
@@ -4,7 +4,8 @@
 ## somebody please help if a better way is discovered :pray
 
 import pydantic
-from bhive.chat import ConverseUsage, ConverseMetrics
+
+from bhive.chat import ConverseMetrics, ConverseUsage
 
 
 class TokenPrices(pydantic.BaseModel):
@@ -14,11 +15,14 @@ class TokenPrices(pydantic.BaseModel):
 
 
 MODELID_COSTS_PER_TOKEN: dict[str, TokenPrices] = {
-    "anthropic.claude-3-haiku-20240307-v1:0": TokenPrices(
-        input_per_1000=0.001, output_per_1000=0.005
-    ),
     "anthropic.claude-3-5-haiku-20241022-v1:0": TokenPrices(
+        input_per_1000=0.008, output_per_1000=0.004
+    ),
+    "anthropic.claude-3-haiku-20240307-v1:0": TokenPrices(
         input_per_1000=0.00025, output_per_1000=0.00125
+    ),
+    "anthropic.claude-3-5-sonnet-20241022-v2:0": TokenPrices(
+        input_per_1000=0.003, output_per_1000=0.015
     ),
     "anthropic.claude-3-5-sonnet-20240620-v1:0": TokenPrices(
         input_per_1000=0.003, output_per_1000=0.015
@@ -26,16 +30,22 @@ MODELID_COSTS_PER_TOKEN: dict[str, TokenPrices] = {
     "anthropic.claude-3-sonnet-20240229-v1:0": TokenPrices(
         input_per_1000=0.003, output_per_1000=0.015
     ),
-    "anthropic.claude-3-5-sonnet-20241022-v2:0": TokenPrices(
-        input_per_1000=0.003, output_per_1000=0.015
-    ),
     "anthropic.claude-3-opus-20240229-v1:0": TokenPrices(
         input_per_1000=0.015, output_per_1000=0.075
     ),
+    "amazon.nova-pro-v1:0": TokenPrices(input_per_1000=0.0008, output_per_1000=0.0032),
+    "amazon.nova-lite-v1:0": TokenPrices(input_per_1000=0.00006, output_per_1000=0.00024),
+    "amazon.nova-micro-v1:0": TokenPrices(input_per_1000=0.000035, output_per_1000=0.00014),
+    "meta.llama3-2-90b-instruct-v1:0": TokenPrices(input_per_1000=0.00072, output_per_1000=0.00072),
+    "meta.llama3-2-11b-instruct-v1:0": TokenPrices(input_per_1000=0.00016, output_per_1000=0.00016),
     "meta.llama3-2-3b-instruct-v1:0": TokenPrices(input_per_1000=0.00015, output_per_1000=0.00015),
+    "meta.llama3-2-1b-instruct-v1:0": TokenPrices(input_per_1000=0.0001, output_per_1000=0.0001),
     "meta.llama3-1-70b-instruct-v1:0": TokenPrices(input_per_1000=0.00099, output_per_1000=0.00099),
     "mistral.mistral-small-2402-v1:0": TokenPrices(input_per_1000=0.001, output_per_1000=0.003),
     "mistral.mistral-large-2402-v1:0": TokenPrices(input_per_1000=0.004, output_per_1000=0.012),
+    "mistral.mistral-large-2402-v1:0": TokenPrices(input_per_1000=0.004, output_per_1000=0.012),
+    "mistral.mistral-7b-instruct-v0:2": TokenPrices(input_per_1000=0.00015, output_per_1000=0.0002),
+    "mistral.mixtral-8x7b-instruct-v0:1" TokenPrices(input_per_1000=0.00045, output_per_1000=0.0007),
 }
 
 
